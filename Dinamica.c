1d9c5fbf8368d8eade76847d3b0b7a5f9d1cd37c
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2021-10-14 12:59:06 -0500

    Sesión 27 - 13/10/2021

---
+++ Dinamica.c
@@ -0,0 +1,13 @@
+// Sesion 25 - 13/10/2021
+
+#include <stdio.h>				//   Para la I/O del programa (entrada / Salida)
+#include <stdlib.h>				//  Para las funcionesde utilería P.E. system();
+#include <malloc.h>				//  Para la asignación (memory allocation) dinamica de memoria
+
+int main()
+{
+	
+
+	system("pause");
+	return 1;
+}
---
+++ Listas.c
@@ -0,0 +1,21 @@
+// Sesion 25 - 13/10/2021
+
+#include <stdio.h>				//   Para la I/O del programa (entrada / Salida)
+#include <stdlib.h>				//  Para las funcionesde utilería P.E. system();
+#include <malloc.h>				//  Para la asignación (memory allocation) dinamica de memoria
+
+struct Nodo 
+{ 
+	char info; 
+	struct Nodo *sig; 
+};
+
+typedef struct Nodo nodo;
+
+int main()
+{
+	nodo caja;
+
+	system("pause");
+	return 1;
+}
