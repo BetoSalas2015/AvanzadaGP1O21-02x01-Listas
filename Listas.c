62633439a8ce0d2cdda5a99c7a7c78d325b430c6
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2021-10-26 12:12:11 -0500

    Sesión 29 parte 2 - 21/10/2021

--- Listas.c
+++ Listas.c
@@ -14,7 +14,7 @@ typedef struct Nodo nodo;
 
 nodo *raiz = NULL;				// Creamos la lista vacia
 
-void insert(char dato)		// dato = 'c'
+void insert2(char dato)		// dato = 'c'
 {
 	nodo *nuevo = NULL;			// Apuntador auxiliar para crear el nuevo nodo
 	nuevo = (nodo *) malloc( sizeof(nodo) );  // Creamos el nuevo nodo
@@ -41,6 +41,30 @@ void insert(char dato)		// dato = 'c'
 	}
 }
 
+void insert1(char dato)		
+{
+	nodo *nuevo = NULL;			// Apuntador auxiliar para crear el nuevo nodo
+	nuevo = (nodo *) malloc( sizeof(nodo) );  // Creamos el nuevo nodo
+	if( nuevo == NULL )			//  Error: Overflow!
+	{
+		printf("Error - Overflow: Ya no hay memoria disponible. \n");
+		system("Pause");
+		return;          
+	}
+	nuevo -> info = dato;		//  Guardamos la información en el nodo
+	nuevo -> sig = NULL;		//  No hay nodo delante..
+	if( raiz == NULL )			//  ¿La lista está vacía?
+	{							//  Verdadero: la lista está vacia
+		raiz = nuevo;
+	}
+	else						//  Falso: la lista no esta vacía (al menos tiene 1 nodo)
+	{
+		nodo *ultimo = raiz;
+		raiz = nuevo;
+		raiz -> sig = ultimo;
+	}
+}
+
 void imprimeLista()
 {
 	nodo *recorre = raiz;
@@ -56,24 +80,24 @@ int main()
 {
 	imprimeLista();
 
-	insert('R');
-	insert('o');
-	insert('b');
-	insert('e');
-	insert('r');
-	insert('t');
-	insert('o');
+	insert1('R');
+	insert1('o');
+	insert1('b');
+	insert1('e');
+	insert1('r');
+	insert1('t');
+	insert1('o');
 
 	imprimeLista();
 
-	insert(' ');
-	insert('S');
-	insert('a');
-	insert('l');
-	insert('a');
-	insert('z');
-	insert('a');
-	insert('r');
+	insert1(' ');
+	insert1('S');
+	insert1('a');
+	insert1('l');
+	insert1('a');
+	insert1('z');
+	insert1('a');
+	insert1('r');
 
     imprimeLista();
 
